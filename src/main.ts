1d2e804228bb7be15bc91dc7104fe3666bb3e00e
Commentator2.0 <kommentator2.9.0.1@gmail.com>
2022-04-20 00:46:11 +0200

    added propper shutdown on unexpected errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { DiscordClient } from "./model/domain/clients/DiscordClient";
 import { TwitchClient } from "./model/domain/clients/TwitchClient";
 import { DB } from "./db";
+import { logger } from "./logger";
 
+async function shutdown(info: number | unknown | Error) {
+    logger.error("Shutting down unexpectedly...");
+    logger.error(`Shutting down with info: ${info instanceof Error ? info.message : info}`);
+    if (info instanceof Error) {
+        logger.debug(info.stack);
+    }
+    discordClient.destroy();
+    await twitchClient.disconnect();
+    //ctx.db.shutdown()
+}
 const discordClient = new DiscordClient();
 const twitchClient = new TwitchClient();
 const db = new DB();
 await Promise.all([discordClient.startup(), twitchClient.startup(), db.startup()]);
+
+process.on("unhandledRejection", shutdown);
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+process.on("uncaughtException", shutdown);
 
 export { discordClient, twitchClient, db };
