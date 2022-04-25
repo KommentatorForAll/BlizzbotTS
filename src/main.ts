22c90994d423333df5ec58db3c5624e9d11aa974
Commentator2.0 <kommentator2.9.0.1@gmail.com>
2022-04-25 21:25:28 +0200

    added repl shell

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { DiscordClient } from "./model/domain/clients/DiscordClient";
 import { TwitchClient } from "./model/domain/clients/TwitchClient";
 import { DB } from "./db";
 import { logger } from "./logger";
+import * as repl from "repl";
+import { config } from "./config";
 
 async function shutdown(info: number | unknown | Error) {
     logger.error("Shutting down unexpectedly...");
@@ -22,5 +24,12 @@ process.on("unhandledRejection", shutdown);
 process.on("SIGTERM", shutdown);
 process.on("SIGINT", shutdown);
 process.on("uncaughtException", shutdown);
+
+const shell = repl.start("> ");
+shell.context.db = db;
+shell.context.discordClient = discordClient;
+shell.context.twitchClient = twitchClient;
+shell.context.config = config;
+shell.context.logger = logger;
 
 export { discordClient, twitchClient, db };
